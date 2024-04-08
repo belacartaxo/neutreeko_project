ae54a4d486ba02dddfedf60b744252d3f764398e
Isabela Britto Cartaxo <belacartaxo@gmail.com>
2024-04-08 01:16:25 +0100

    Update game.py

--- neutreeko/game.py
+++ neutreeko/game.py
@@ -1,6 +1,7 @@
 import random
 import pygame
 import sys
+import os
 from .board import Board
 from .piece import Piece
 
@@ -9,25 +10,27 @@ SIZE =  5
 WHITE = (255, 255, 255) 
 BLACK = (0, 0, 0)
 GREEN = (78,255,93)
-BLUE_1 = (88, 154, 141) 
-BLUE_2 = (143, 193, 181)
+GREEN_1 = (88, 154, 141) 
+GREEN_2 = (143, 193, 181)
+GREEN_3 = (23,101,81)
 CELL_SIZE = 100
+BOARD_EDGE = 10
 BOARD_SIZE = SIZE * CELL_SIZE
-WIDTH_BUTTON = 300
-HEIGHT_BUTTON = 100
+SPACE_SCREEN_BOARD = 100
+SCREEN_SIZE = BOARD_SIZE + SPACE_SCREEN_BOARD
+WIDTH_BUTTON = 250
+HEIGHT_BUTTON = 80
+WIDTH_BOX = 300
+HEIGHT_BOX = 100
 TIME = 300
 PIECE_COLORS = (BLACK, WHITE)
-# --------- Graph ----------#
+FONT_PATH = os.path.join("assets", "font", "Bungee_Poppins", "Bungee", "Bungee-Regular.ttf")
 
 class NeutreekoGame:
     def __init__(self, ai1 = None, ai2 = None):
-        # Initial positions for player 1 and player 2
         self.initial_position = [(4, 1),(1, 2), (4, 3)], [(0,1), (3, 2), (0,3)]
-        # Create a new board with the initial positions
         self.board = Board(self.initial_position)
-        # Player 1 and Player 2 functions to control their moves
         self.player =[ ai1, ai2]
-        # Flags to control the game flow
         self.button_clicked = False
         self.screen_update = False
         self.mouse_over_button = False
@@ -36,17 +39,16 @@ class NeutreekoGame:
         self.available_pieces = []
         
     def create_board_surface(self, screen):
-        # Create the game board surface
+        pygame.draw.rect(screen, BLACK, (SPACE_SCREEN_BOARD/2-BOARD_EDGE,SPACE_SCREEN_BOARD/2-BOARD_EDGE, BOARD_SIZE+BOARD_EDGE*2, BOARD_SIZE+BOARD_EDGE*2))
         for row in range(SIZE):
             for col in range(SIZE):
-                color =  BLUE_1 if (row + col) % 2 == 0 else BLUE_2 # Determine the color of the current cell
-                pygame.draw.rect(screen, color, (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE))  #Draw a rectangle representing the current cell on the screen
+                color =  GREEN_1 if (row + col) % 2 == 0 else GREEN_2
+                pygame.draw.rect(screen, color, (SPACE_SCREEN_BOARD/2+col * CELL_SIZE, SPACE_SCREEN_BOARD/2+row * CELL_SIZE, CELL_SIZE, CELL_SIZE))
 
     def create_piece_surface(self, pieces, screen, clicked_piece):
         for i in range(len(pieces)):
             if len(self.game_pieces[i]) == 0:
                 self.game_pieces[i] = [self.create_piece(p, i+1, PIECE_COLORS[i], screen) for p in pieces[i]]
-
             else:
                 self.draw_piece(self.game_pieces[i], screen)
                       
@@ -62,20 +64,22 @@ class NeutreekoGame:
             p.draw(screen)
     
     def create_piece(self, piece, player, color, screen, father = None):
-        piece = Piece(self.board, piece, player, color, CELL_SIZE, father)
+        piece = Piece(self.board, piece, player, color, CELL_SIZE, SPACE_SCREEN_BOARD/2, father)
         piece.draw(screen)
         return piece
             
-    def update_screen(self, screen, clicked_piece = None):
-        # Update the game screen with the board and pieces
+    def update_board_screen(self, screen, clicked_piece = None):
         self.create_board_surface(screen)
         self.create_piece_surface(self.board.pieces, screen, clicked_piece)
         pygame.display.flip()
+
+    def update_home_screen(self, screen):
+        pygame.draw.rect(screen, GREEN_1, (0, 0, SCREEN_SIZE, SCREEN_SIZE))
     
     def check_click(self, screen, pos):
         for piece in self.game_pieces[self.board.current_player-1]:
             if piece.is_clicked(pos):
-                self.update_screen(screen, piece) 
+                self.update_board_screen(screen, piece) 
                 return
     
         for piece in self.available_pieces:
@@ -88,80 +92,78 @@ class NeutreekoGame:
 
         
     def run_game(self):
-        # Initialize the game board
         self.board = Board(self.initial_position)
+        pygame.init()
+        screen = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
+        pygame.display.set_caption("Neutreeko")
+        font_1 = pygame.font.Font(FONT_PATH, 30)
+        font_2 = pygame.font.Font(FONT_PATH, 60)
+        start_button_rect = pygame.Rect((SCREEN_SIZE - WIDTH_BUTTON)/2, 200, WIDTH_BUTTON, HEIGHT_BUTTON)
+        rules_button_rect = pygame.Rect((SCREEN_SIZE - WIDTH_BUTTON)/2, 300, WIDTH_BUTTON, HEIGHT_BUTTON)
+        obs_button_rect = pygame.Rect((SCREEN_SIZE - WIDTH_BUTTON)/2, 400, WIDTH_BUTTON, HEIGHT_BUTTON)
 
-        # GRAPH
-        pygame.init()# Initialize Pygame
-        screen = pygame.display.set_mode((BOARD_SIZE, BOARD_SIZE))# Set up the game screen 
-        pygame.display.set_caption("Neutreeko")# Set the title of the game window
-        font = pygame.font.SysFont("Arial", 50) # Create a font object for rendering text in the game
-        button_rect = pygame.Rect(100, 200, WIDTH_BUTTON, HEIGHT_BUTTON)# Create a rectangle representing the button
-
-        # Enter a loop to continuously handle Pygame events
         while True: 
-            for event in pygame.event.get(): # Iterate through all Pygame events
-                if event.type == pygame.QUIT:   # Check if the user clicked the close button
-                    pygame.quit() # Quit Pygame
-                    sys.exit() # Exit the Python program
-                elif event.type == pygame.MOUSEBUTTONDOWN:# Check if the user clicked the mouse button
-                    if not self.button_clicked and button_rect.collidepoint(event.pos):  # Check if the game button hasn't been clicked yet and if the mouse click occurGREEN within the button rectangle
-                        self.button_clicked = True # Set the button_clicked flag to True
-                        self.update_screen(screen)# Update the screen to reflect the button click
-                    elif self.button_clicked and self.player[self.board.current_player-1] is None:  # Check if current player is human
-                    # Process human player's click
+            for event in pygame.event.get(): 
+                if event.type == pygame.QUIT:   
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if not self.button_clicked and start_button_rect.collidepoint(event.pos):
+                        self.button_clicked = True
+                        self.update_board_screen(screen)
+                    elif self.button_clicked and self.player[self.board.current_player-1] is None:
                         mouse_pos = pygame.mouse.get_pos()
                         self.check_click(screen, mouse_pos)
 
             if self.button_clicked:
-                # Execute player moves
-                if self.player[self.board.current_player-1]: # se o jogador for uma ia
+                if self.player[self.board.current_player-1]:
+
                     if self.board.current_player == 1:
                         self.player[0](self)
                     else:
                         self.player[1](self)   
-                    pygame.time.wait(TIME) # wait a few seconds so we can view the moves
+                    pygame.time.wait(TIME)
                     self.players_moved = True
-                self.update_screen(screen) # updates the screen after each movement
-                # Check the winner
-        
+                self.update_board_screen(screen)
+
                 if self.players_moved: 
                     if self.board.winner != -1:
-                        text = f"Player {self.board.winner} wins!" if self.board.winner != 0 else "Draw!" # Determine the message to be displayed based on the winner
-                        text_surface = font.render(text, True, BLACK)  # Render the text onto a surface
-                        text_rect = self.create_text(screen, font, text)   # Create a rectangle to position the text in the center of the screen
-                        screen.blit(text_surface, text_rect) # Blit the rendeGREEN text surface onto the screen
-                        pygame.display.flip()   # Update the display 
-                        pygame.time.wait(2000) # Pause the game so we can see the result
-                        pygame.quit() # Quit Pygame
-                        return  # Exit the function, ending the game
-                    # Switch player turn
+                        text = f"Player {self.board.winner} wins!" if self.board.winner != 0 else "Draw!"
+                        self.create_text(screen, text, font_1, WHITE, ((SCREEN_SIZE - WIDTH_BOX)/2, (SCREEN_SIZE - HEIGHT_BOX )/2), True)
+                        pygame.time.wait(2000)
+                        pygame.quit()
+                        return  
                     self.board.current_player = 3 - self.board.current_player
                     self.players_moved = False
             
-            # If the screen has not been updated yet
             if not self.screen_update:
-                self.update_screen(screen) # Update the screen with the game board and pieces
-                self.draw_button(screen, font, button_rect)  # Draw the start game button on the screen
-                self.screen_update = True # Set the screen_update flag to True to indicate that the screen has been updated
+                self.update_home_screen(screen)
+                self.create_text(screen, "NEUTREEKO", font_2, WHITE, 100)
+                self.draw_button(screen, 'Start Game', font_1, start_button_rect,(SCREEN_SIZE/2, start_button_rect.y))
+                self.draw_button(screen, 'Rules', font_1, rules_button_rect,((SCREEN_SIZE/2, rules_button_rect.y)))
+                self.draw_button(screen, 'Information', font_1, obs_button_rect,((SCREEN_SIZE/2, obs_button_rect.y)))
+                self.screen_update = True
 
-        
-    def create_text(self, screen, font, text):
-        # Create a text surface
-        result_box = pygame.Rect(100, 200, 300, 100) # Define a rectangle
-        pygame.draw.rect(screen, WHITE, result_box) # Draw a rectangle on the screen using the defined rectangle
-        text = font.render(text, True, WHITE) # Render the text onto a surface
-        return text.get_rect(center=result_box.center) # Return a rectangle that centers the rendeGREEN text within the previously defined rectangle
-    
-    def draw_button(self, screen, font, button_rect):   
-            # Draw the start game button         
-            pygame.draw.rect(screen, BLACK, button_rect) # Draw a rectangle representing the button on the scree
-            button_text = font.render("Start  Game", True, WHITE) # Render the text "Start Game" onto a surface
-            screen.blit(button_text, (140, 220)) # Blit the rendeGREEN text surface onto the screen
-            pygame.display.flip()# Update the display
+    def create_text(self, screen, text, font, color_text, position, box=False):
+        #text_rect = text_surface.get_rect()
+        text_surface = font.render(text, True, color_text)
+        text_width, text_height = text_surface.get_size()
+        if box:
+            text_rect = pygame.Rect(position[0], position[1], WIDTH_BOX, HEIGHT_BOX)
+            pygame.draw.rect(screen, BLACK, text_rect)
+            screen.blit(text_surface, ((SCREEN_SIZE - text_width)/2, (SCREEN_SIZE - text_height )/2))
+        else:
+            screen.blit(text_surface, ((SCREEN_SIZE - text_width)/2, position))
+        pygame.display.flip()
+
+    def draw_button(self, screen, text, font, button_rect, position):   
+        pygame.draw.rect(screen, GREEN_3, button_rect, 0, 10)
+        button_surface = font.render(text, True, WHITE)
+        text_width, text_height = button_surface.get_size()
+        screen.blit(button_surface, (position[0]-text_width/2, position[1]+ (HEIGHT_BUTTON - text_height)/2))
+        pygame.display.flip()
 
 def execute_random_move(game):
-    # Select a random move among the available moves in the game
     new_pieces = random.choice(game.board.available_moves())
-    # Execute the selected move on the game board
+
     game.board.move(new_pieces[0], new_pieces[1])
