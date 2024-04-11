dfd941e946e4c213c6096435d45c8ee442d33f21
git/Isabela Cartaxo <belacartaxo@gmail.com>
2024-04-11 22:45:28 +0100

    ATT

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from ai.heuristics import *
 # Define the main function of the program
 def main():
     # Create an instance of the NeutreekoGame, passing execute_random_move as the function to control moves for both players
-    game = NeutreekoGame(execute_minimax_alpha_beta_move(evaluate_f1 , 3),execute_minimax_alpha_beta_move(evaluate_f1, 3))
+    game = NeutreekoGame(None, execute_minimax_alpha_beta_move(evaluate_f4, 4))
     game.run_game() # Start the game
 
 main()# Call the main function to start the program
